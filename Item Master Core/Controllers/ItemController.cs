93e85a651d3739b019857b0baf165b8036a33e88
Terry Huang <Terry.Huang@treeoflife.com>
2019-01-31 11:30:27 -0500

    Add project files.

---
+++ Item Master Core/Controllers/ItemController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Item_Master_Core.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Item Master Core/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Item_Master_Core.MvcApplication" Language="C#" %>
