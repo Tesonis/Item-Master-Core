877f4e2408ee4c9eab6fc67e393ad4ac63b05b3b
Tesonis <tl_huang@hotmail.com>
2019-02-13 22:27:56 -0500

    Added grid view list view toggle on index

---
+++ Item Master Core/Scripts/search-view-toggle.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $("#listview").hide();
+});
+
+$(".grid-view-btn").click(function () {
+    if ($(".grid-view-btn").hasClass("btn-outline-secondary")) {
+        $("#gridview").toggle();
+        $("#listview").toggle();
+        $(".grid-view-btn").removeClass("btn-outline-secondary");
+        $(".grid-view-btn").addClass("btn-secondary");
+        $(".list-view-btn").removeClass("btn-secondary");
+        $(".list-view-btn").addClass("btn-outline-secondary");
+    }  
+});
+
+$(".list-view-btn").click(function () {
+    if ($(".list-view-btn").hasClass("btn-outline-secondary")) {
+        $("#listview").toggle();
+        $("#gridview").toggle();
+        $(".list-view-btn").removeClass("btn-outline-secondary");
+        $(".list-view-btn").addClass("btn-secondary");
+        $(".grid-view-btn").removeClass("btn-secondary");
+        $(".grid-view-btn").addClass("btn-outline-secondary");
+    }
+});
